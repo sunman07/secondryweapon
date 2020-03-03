5aa76e71e12356f9a7775a4d43e79231a84cba9a
yangyanhao <576217481@qq.com>
2020-03-03 11:17:20 +0800

    tijiao

--- src/lib/request.js
+++ src/lib/request.js
@@ -12,7 +12,7 @@ axios.interceptors.request.use(function(config) {
     // 在发送请求之前做些什么
     console.debug('发出请求');
      config.headers.AccessToken =
-       window["__AppWebkey"] || "ABYPAEWPMLM9DLM1F3ANBQ";
+       window["__AppWebkey"] || "WDPRRV_APYE-2UNCRF4LPQ";
     return config;
 }, function(error) {
     // 对请求错误做些什么
